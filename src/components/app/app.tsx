1f319cf48f41e3ba2f6cd1915b2ed21bc8f23faa
Andrey <kaliushka890@gmail.com>
2024-02-17 23:55:37 +0700

    Добавляет страницу Логина

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,12 +1,13 @@
 import MainScreen from '../../pages/main-screen/main-screen';
 
+
 type AppProps = {
   placeCardCount: number;
 }
 
-function App ({placeCardCount}: AppProps) {
+function App ({placeCardCount}: AppProps): JSX.Element {
   return (
-    <MainScreen placeCardCount = {placeCardCount}/>
+    <MainScreen placeCardCount = {placeCardCount} />
   );
 }
 
