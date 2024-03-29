b076d1bfa3b3b6bf0616d8227a536e81c416a6b5
Andrey <kaliushka890@gmail.com>
2024-03-29 19:48:46 +0700

    опять неведомая ошибка гита

--- src/components/hooks/use-auth.ts
+++
@@ -1,9 +0,0 @@
-import { AuthorizationStatus } from '../../const';
-import { useAppSelector } from '../store/types';
-
-function useAuth() {
-  const authStatus = useAppSelector((state) => state.authStatus);
-  return {isAuth: authStatus === AuthorizationStatus.Auth};
-}
-
-export default useAuth;
